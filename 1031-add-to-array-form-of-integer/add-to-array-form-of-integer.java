ccab97393c736289628ea2c70f314a5ef29b38f9
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-06-27 22:03:04 +0530

    Time: 4 ms (30.45%) | Memory: 45.6 MB (94.17%) - LeetSync

---
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        
+        int i=num.length-1;
+        int carray=0;
+        int mod=0;
+        List<Integer> ans=new ArrayList<>();
+        int val=k;
+
+        while(i>=0){
+            //System.out.println("...................");
+            mod=val%10;
+            val=val/10;
+            int sum= mod+num[i];
+            ans.add(sum%10);
+            carray=sum/10;
+            val+=carray;
+            //System.out.println("mod: "+mod+" :: val: "+val+" :: sum: "+sum+" ::carray: "+carray);
+            i--;
+        }
+        
+        while(val>0){
+            //System.out.println("--"+carray);
+            ans.add(val%10);
+            val/=10;
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}

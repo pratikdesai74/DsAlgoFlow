06bc07a52f56464005959b50285ee278aa8c2ee1
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-07-13 21:16:49 +0530

    Time: 24 ms (73.22%) | Memory: 46.4 MB (65.43%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> runningSumFrequency= new HashMap<>();
+        int sum=0;
+        int countOfSubarray=0;
+
+        runningSumFrequency.put(0,1);
+
+        for(int num:nums){
+
+            sum+=num;
+            int runningSumMatchingKey = sum-k; 
+            // int frequency = runningSumFrequency.containsKey(runningSumMatchingKey);
+
+            if(runningSumFrequency.containsKey(runningSumMatchingKey)){
+               countOfSubarray+= runningSumFrequency.get(runningSumMatchingKey);
+            }
+
+            runningSumFrequency.put(sum,runningSumFrequency.getOrDefault(sum,0)+1);
+        }
+
+        return countOfSubarray;
+    }
+}

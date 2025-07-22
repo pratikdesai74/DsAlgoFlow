927831138b122c2ff52956ad8aa5390ad13c5418
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-07-23 00:55:09 +0530

    Time: 2 ms (97.26%) | Memory: 41.7 MB (73.00%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,22 +1,22 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        List<Integer> nums=new ArrayList<>();
-        nums.add(1);
+        int[] uglyNumbers=new int[n];
+        uglyNumbers[0]=1;
         int p2=0,p3=0,p5=0;
 
-        while(nums.size()<n){
-            int twoMultiple= nums.get(p2)*2;
-            int threeMultiple= nums.get(p3)*3;
-            int fiveMultiple= nums.get(p5)*5;
+        for(int i=1;i<n;i++){
+            int twoMultiple= uglyNumbers[p2]*2;
+            int threeMultiple= uglyNumbers[p3]*3;
+            int fiveMultiple= uglyNumbers[p5]*5;
 
             int minMultiple=Math.min(twoMultiple,Math.min(threeMultiple,fiveMultiple));
-            nums.add(minMultiple);
-
+            uglyNumbers[i]=minMultiple;
+            
             if(minMultiple==twoMultiple) p2++;
             if(minMultiple==threeMultiple) p3++;
             if(minMultiple==fiveMultiple) p5++;
         }
 
-        return nums.get(n-1);
+        return uglyNumbers[n-1];
     }
 }

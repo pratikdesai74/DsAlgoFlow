c34865d14ebdec45849275c7cb97ed8903839faf
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-07-26 22:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (82.41%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -8,6 +8,8 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
+        // it is not possible to delete node, if we dont have address of previous node or head node
+        // so we are copying next node value and deleteing next node
         node.val=node.next.val;
         node.next=node.next.next;
     }

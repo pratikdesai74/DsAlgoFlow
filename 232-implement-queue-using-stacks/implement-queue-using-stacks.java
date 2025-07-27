a74bafd59991d8f86edbf9fc94d3004d1c3bf11a
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-07-27 14:47:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (37.92%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,39 +1,79 @@
+//method 1(teachers approach):  push always in input queue and for pop/peek use output queue 
+//if output queue is empty while peek/pop, (ulti bottel) push all elelments till now
+//in input queue into output queue and the do peek /pop 
 class MyQueue {
-    public Stack<Integer> stkMain;
-    public Stack<Integer> stkSupport;
+    public Stack<Integer> output;
+    public Stack<Integer> input;
     public MyQueue() {
-        stkMain=new Stack<>();
-        stkSupport=new Stack<>();
+        output=new Stack<>();
+        input=new Stack<>();
     }
     
-    public void push(int x) {
-        while(!stkMain.empty()){
-            int topElement= stkMain.peek();
-            stkMain.pop();
-            stkSupport.push(topElement);
-        }
-        stkMain.push(x);
+    public void fillOutputWithInputStack(){ 
+        if(output.empty()){
+            while(!input.empty()){
+                int topElement= input.pop();
+                output.push(topElement);
+            }
+        }    
+    }
 
-        while(!stkSupport.empty()){
-            int topElement= stkSupport.peek();
-            stkSupport.pop();
-            stkMain.push(topElement);
-        }
+    public void push(int x) {
+        input.push(x);
     }
     
     public int pop() {
-        return stkMain.pop();
+        fillOutputWithInputStack();
+        return output.pop();
     }
     
     public int peek() {
-        return stkMain.peek();
+        fillOutputWithInputStack();
+        return output.peek();
     }
     
     public boolean empty() {
-        return stkMain.empty();
+        return input.empty() && output.empty();
     }
 }
 
+//method 2:  maintin queue like order while pushing the element (our approach)
+// class MyQueue {
+//     public Stack<Integer> stkMain;
+//     public Stack<Integer> stkSupport;
+//     public MyQueue() {
+//         stkMain=new Stack<>();
+//         stkSupport=new Stack<>();
+//     }
+    
+//     public void push(int x) {
+//         while(!stkMain.empty()){
+//             int topElement= stkMain.peek();
+//             stkMain.pop();
+//             stkSupport.push(topElement);
+//         }
+//         stkMain.push(x);
+
+//         while(!stkSupport.empty()){
+//             int topElement= stkSupport.peek();
+//             stkSupport.pop();
+//             stkMain.push(topElement);
+//         }
+//     }
+    
+//     public int pop() {
+//         int topElement= stkMain.peek();
+//         return stkMain.pop();
+//     }
+    
+//     public int peek() {
+//         return stkMain.peek();
+//     }
+    
+//     public boolean empty() {
+//         return stkMain.empty();
+//     }
+// }
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();

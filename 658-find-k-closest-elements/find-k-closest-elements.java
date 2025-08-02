fc1c3e1ba87751c06bf72f216a964b6f7b6376de
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-08-02 17:41:09 +0530

    Time: 3 ms (99.65%) | Memory: 46.4 MB (13.96%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,18 +1,42 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int start=0,end=arr.length-1;
+// 2 pointer :: T.C= O(n)
 
-        while(end-start>=k){
-            if(Math.abs(arr[start]-x) > Math.abs(arr[end]-x)){
-                start++;
+    // public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    //     int start=0,end=arr.length-1;
+
+    //     while(end-start>=k){
+    //         if(Math.abs(arr[start]-x) > Math.abs(arr[end]-x)){
+    //             start++;
+    //         }
+    //         else{
+    //             end--; 
+    //         }
+    //     }
+
+    //     List<Integer> closestNums=new ArrayList<>(k);
+    //     for(int i=start;i<=end;i++)
+    //         closestNums.add(arr[i]);
+
+    //     return closestNums;
+    // }
+
+//binary search + sliding window :: T.C= O(log(n-k))
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left=0,right=arr.length-k;
+
+        while(left<right){
+            int mid= (left+right)/2;
+
+            if(x-arr[mid] > arr[mid+k]-x){
+                left=mid+1;
             }
             else{
-                end--; 
+                right=mid; 
             }
         }
 
         List<Integer> closestNums=new ArrayList<>(k);
-        for(int i=start;i<=end;i++)
+        for(int i=left;i<left+k;i++)
             closestNums.add(arr[i]);
 
         return closestNums;

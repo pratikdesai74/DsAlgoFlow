f2fbf70b0b6a35e6ef739ceef526a49b2cabb54c
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-09-09 21:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (64.92%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    //DP - Memoization
+    public int fib(int n) {
+        int[] dp=new int[n+1];
+        for(int i=0;i<=n;i++)
+            dp[i]=-1;
+
+        return helper(n,dp);  
+    }
+
+    public int helper(int n,int[] dp){
+        if(n <= 1)
+            return n;
+
+        if(dp[n] != -1)
+            return dp[n];
+
+
+        return dp[n]=helper(n-1,dp) + helper(n-2,dp);       
+    }
+}

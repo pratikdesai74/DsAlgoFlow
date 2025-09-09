ed512b9dec1297e95e6865de77881ce7959a5fda
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-09-09 21:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (38.31%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,21 +1,35 @@
 class Solution {
-    //DP - Memoization
+    // //DP - Memoization
+    // public int fib(int n) {
+    //     int[] dp=new int[n+1];
+    //     for(int i=0;i<=n;i++)
+    //         dp[i]=-1;
+
+    //     return helper(n,dp);  
+    // }
+
+    // public int helper(int n,int[] dp){
+    //     if(n <= 1)
+    //         return n;
+
+    //     if(dp[n] != -1)
+    //         return dp[n];
+
+
+    //     return dp[n]=helper(n-1,dp) + helper(n-2,dp);       
+    // }
+
+    //DP - Tabulation(real dp)
     public int fib(int n) {
         int[] dp=new int[n+1];
-        for(int i=0;i<=n;i++)
-            dp[i]=-1;
 
-        return helper(n,dp);  
-    }
+        for(int i=0;i<=n;i++){
+            if(i==0 || i==1)
+                dp[i]=i;
+            else    
+                dp[i]=dp[i-1]+dp[i-2];
+        }
 
-    public int helper(int n,int[] dp){
-        if(n <= 1)
-            return n;
-
-        if(dp[n] != -1)
-            return dp[n];
-
-
-        return dp[n]=helper(n-1,dp) + helper(n-2,dp);       
+        return dp[n];  
     }
 }

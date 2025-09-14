6e9d9e65c1c84f3340a5574d8747d238a3c429a3
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-09-14 20:22:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (24.76%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,16 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-        int[]dp = new int[nums.length];
-        dp[0]=nums[0];
+        /*
+        dp[i] : max amount of money that the robber can rob till i-th index
+        */
         int n=nums.length;
+        int[]dp = new int[n];
         
         if(n==1)
-            return dp[0];
+            return nums[0];
 
+        dp[0]=nums[0];
         dp[1]= Math.max(dp[0],nums[1]);    
 
         for(int i=2; i<n; i++){
-            dp[i]= Math.max(dp[i-1], dp[i-2]+nums[i]);
+            int pick = nums[i] + dp[i-2];
+            int notPick = dp[i-1];
+            dp[i]= Math.max(pick, notPick);
         }
 
         return dp[n-1];

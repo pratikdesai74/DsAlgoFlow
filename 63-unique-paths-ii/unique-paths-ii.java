5733e2362b7fafac8c9475f0a5af4f5a767d11d8
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-09-16 01:30:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (6.39%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        /*
+        dp[i][j] --> number of path to reach [m-1][n-1] cell from [i][j] cell
+    i.e dp[0][0] --> number of path to reach [m-1][n-1] cell from [0][0] cell    
+        */
+        int rows= obstacleGrid.length;
+        int cols=obstacleGrid[0].length;
+
+        int[][] dp=new int[rows][cols];
+        
+        if(obstacleGrid[0][0]==1 || obstacleGrid[rows-1][cols-1]==1)
+            return 0;
+
+        // filling the last column as base condition
+        for(int i=rows-1; i>=0; i--){
+            if(obstacleGrid[i][cols-1] ==1){
+            /*    
+             when we found the obstacle making every cell from that cell to its left as 0, because all the cells from that cell to left of it, wont be able to traverse to the destination cell
+            */ 
+                break;
+            }
+            else{
+                dp[i][cols-1]=1;
+            }    
+        }
+
+        // filling the last row as base condition
+        for(int j=cols-1; j>=0; j--){
+            if(obstacleGrid[rows-1][j] ==1){
+                /*    
+             when we found the obstacle making every cell from that cell to its top as 0, because all the cells from that cell to top of it, wont be able to traverse to the destination cell
+            */ 
+                break;
+            }
+            else{
+                dp[rows-1][j]=1;
+            }       
+        }
+
+
+        for(int i=rows-2; i>=0; i--){
+            for(int j=cols-2; j>=0; j--){
+                if(obstacleGrid[i][j] !=1){
+                    dp[i][j]= dp[i+1][j] + dp[i][j+1];
+                }
+                else{
+                   dp[i][j]=0; 
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}

c785fa12e34b788ad6b1e2662669aef6f63c1290
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-09-17 23:34:37 +0530

    Time: 20 ms (73.65%) | Memory: 50.9 MB (57.11%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -2,20 +2,40 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int l1=text1.length(); //rows
         int l2=text2.length(); //cols
+        /*
+        dp[i][j] --> Longest common subsequence (LCS) of text1 till length i, with text2 till length j
+        dp[l1][l2] --> Longest common subsequence (LCS) of text1 till length l1, with text2 till length l2
+        */
+        
+        /*
+        Base case: 
+        first row and first columns all values are to be kept 0, as for 0 length of either text1 with any length of text2
+        or vise versa, there wont be any common char 
+        */
 
         int[][] dp=new int[l1+1][l2+2];
 
         for(int i=1; i<=l1; i++){
             for(int j=1; j<=l2; j++){
                 if(text1.charAt(i-1) == text2.charAt(j-1)){
-                    dp[i][j]= 1+ dp[i-1][j-1];
+                    /*
+                    if current char matches from text1 with same index char from text2
+                    increase length of LCS by 1 in previous diagonal cell
+                    */
+                    dp[i][j]= 1+ dp[i-1][j-1]; // 1+ diagonally previous cell
                 }
                 else{
-                    int condition1= dp[i-1][j];
-                    int condition2= dp[i][j-1];
-                    int condition3= dp[i-1][j-1];
+                    /*
+                    if current char from text1 dont match with same index char from text2
+                    we wont increment the LCS length,
+                    but moving forword we need to decide(Maximum out of all 3 cases) and update the value
+                    */
 
-                    dp[i][j]=Math.max( Math.max(condition1,condition2),condition3);
+                    int case1= dp[i-1][j]; //top of current cell
+                    int case2= dp[i][j-1]; //left of current cell
+                    int case3= dp[i-1][j-1]; // diagonally previous cell
+
+                    dp[i][j]=Math.max( Math.max(case1,case2),case3);
                 }
             }
         }

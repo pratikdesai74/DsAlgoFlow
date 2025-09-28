4b7a95242c3cf946d734ae5f920929c4607db9b3
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-09-28 18:36:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (82.47%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -16,7 +16,7 @@ class Solution {
         for(int i=rows-1; i>=0; i--){
             if(obstacleGrid[i][cols-1] ==1){
             /*    
-             when we found the obstacle, make every cell from that cell to its left as 0, because all the cells from that cell to left of it, wont be able to traverse to the destination cell
+             when we found the obstacle, make every cell from that cell to its top as 0, because all the cells from that cell to its top of it, wont be able to traverse to the destination cell
             */ 
                 break;
             }
@@ -29,7 +29,7 @@ class Solution {
         for(int j=cols-1; j>=0; j--){
             if(obstacleGrid[rows-1][j] ==1){
                 /*    
-             when we found the obstacle, make every cell from that cell to its top as 0, because all the cells from that cell to top of it, wont be able to traverse to the destination cell
+             when we found the obstacle, make every cell from that cell to its left as 0, because all the cells from that cell to left of it, wont be able to traverse to the destination cell
             */ 
                 break;
             }
@@ -41,11 +41,11 @@ class Solution {
 
         for(int i=rows-2; i>=0; i--){
             for(int j=cols-2; j>=0; j--){
-                if(obstacleGrid[i][j] !=1){
+                if(obstacleGrid[i][j] !=1){ // current cell is not having obstacle 
                     dp[i][j]= dp[i+1][j] + dp[i][j+1];
                 }
                 else{
-                   dp[i][j]=0; 
+                   dp[i][j]=0; // current cell is having obstacle 
                 }
             }
         }

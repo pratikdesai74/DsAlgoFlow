fc365557bcefb023b99f1e36f2bd8faf0d0446d3
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-10-03 23:39:18 +0530

    Time: 12 ms (15.52%) | Memory: 60.7 MB (27.51%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,26 +1,44 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
+        /*
+        This question asks to get maximum length of square matrix, and this question asks about maximum area
+        */
         int m = matrix.length;
         int n = matrix[0].length;
-        int maxSide=0; // length or breadth
+        int MaxLengthOfMatrix=0; // length or breadth of square matrix
 
+        /*
+        dp[i][j] : maximum length of square till cell [i][j] (but its left top cell can be any cell, not specifically need to be cell[0][0]
+        so we need to keep track of maximum length/side of square matrix we encounter while traversing from cell [0][0] till [m-1][n-1]
+        */
         int dp[][] = new int[m][n];
         
-        // base case : first column
+        /*
+        base case : first column
+        as they cant have more than 1 cell at a time as a square matrix, we can set same values in dp, what we have in input matrix
+        and also will keep track of maxSide/maxLength of square matrix
+        */
         for(int i=0; i<m; i++){
             dp[i][0] = Character.getNumericValue(matrix[i][0]);
-            maxSide=Math.max(maxSide, dp[i][0]);
-            System.out.println(dp[i][0]+" | ");
+            MaxLengthOfMatrix=Math.max(MaxLengthOfMatrix, dp[i][0]);
         }
-        System.out.println("---------------------------------");
-        // base case : first row
+
+        // base case : first row :: same as column
         for(int j=0; j<n; j++){
             dp[0][j] = Character.getNumericValue(matrix[0][j]);
-            maxSide=Math.max(maxSide, dp[0][j]);
-            System.out.print(dp[0][j]+" | ");
+            MaxLengthOfMatrix=Math.max(MaxLengthOfMatrix, dp[0][j]);
         }
-        System.out.println("");
-        System.out.println("---------------------------------");
+
+        /*
+        The main condition we check: if the current matrix cell is greater than 0 i.e 1, if its 0 then it cant be counted in.
+
+        and if current matrix cell is 1 ,
+            then : dp[i][j] = 1+ min (leftCell, topCell, diagonallyPreviousCell)
+
+        also we will be maintaing the maxSide/MaxLength 
+        so we get maxArea =  MaxLength * MaxLength    
+        */
+
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 if(Character.getNumericValue(matrix[i][j]) != 0){
@@ -29,15 +47,12 @@ class Solution {
                     int diagonallyLeftCell = dp[i-1][j-1];
                     
                     dp[i][j] = 1+ Math.min(diagonallyLeftCell, Math.min(leftCellValue,topCellValue));
-                    maxSide=Math.max(maxSide, dp[i][j]);
+                    MaxLengthOfMatrix=Math.max(MaxLengthOfMatrix, dp[i][j]);
                 }
-                System.out.print(dp[i][j]+" | ");
             }
-            System.out.println("");
-            System.out.println("---------------------------------"+i+" th row done");
         }
         
         
-        return maxSide * maxSide;
+        return MaxLengthOfMatrix * MaxLengthOfMatrix;
     }
 }

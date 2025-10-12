5296135c5fbd1a64ca5a72281382c880cd930eb3
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-10-12 23:33:39 +0530

    Time: 40 ms (67.21%) | Memory: 44.5 MB (50.31%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -13,8 +13,16 @@ class Solution {
          */
         
         int[] dp= new int[n];
+        // base case dp[0]=1 , as there isnt any number before this, so lis for 0th index is 1
         dp[0]=1;
 
+        /*
+         1. for every dp[i] we will add 1 to previousBestLisFoundSoFar
+         2. to find previousBestLisFoundSoFar we will look for Lis of every previous numbers which are smaller than current number
+         i.e nums[j] < nums[i] ---> this gives only incresing subsequences
+         this will make sure that we will add 1 (as a contribution of current number) in a previousBestLisFoundSoFar to get the longest value
+         3. While doing this we will always maintain maximumLis so that we can return it at last.
+         */
         for(int i=1; i<n;i++){
             int previousBestLisFoundSoFar = 0;
 

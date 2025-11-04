8f0fc80ee1e3486d7594a26d9e743b218a725a99
Pratik Vilas Desai <46866019+pratikdesai74@users.noreply.github.com>
2025-11-04 18:16:12 +0530

    Time: 4 ms (49.00%) | Memory: 54.5 MB (5.07%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,15 +1,19 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int numOfIslands=0;
-        int row=grid.length;
-        int col=grid[0].length;
+        int m=grid.length;
+        int n=grid[0].length;
 
-        int[][] visited = new int[row][col];
+        int[][] visited = new int[m][n];
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(visited[i][j] == 0 && grid[i][j]== '1'){
-                    dfs(grid,i,j,visited);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                /* conditions:
+                1. cell should contain '1'
+                2. that cell should not be already visited
+                */
+                if(grid[i][j]== '1' && visited[i][j] == 0){
+                    dfs(i,j,m,n,visited,grid);
                     numOfIslands+=1;
                 }
             }
@@ -18,22 +22,32 @@ class Solution {
         return numOfIslands;
     }
 
-    public void dfs(char[][] grid, int row,int col, int[][] visited){
-        //These will give us top,left,down,right field from matrix when we will add these values to current i and j values
-        int rowSize= visited.length;
-        int colSize= visited[0].length;
+    public void dfs(int i,int j, int m, int n, int[][] visited, char[][] grid){
+        // mark it as visited
+        visited[i][j]=1;
 
+        /*
+        Explore all 4 direction for current cell (i,j) --> top, left,down, right
+        These will give us top,left,down,right cell from matrix when we will add these values to current i and j values
+        */
         int[] x= {-1,0,1,0}; 
         int[] y= {0,-1,0,1};
 
-        visited[row][col]=1;
-
-        for(int i=0;i<4;i++){
-            int r= row + x[i];
-            int c= col + y[i];
+        for(int k=0;k<4;k++){
+            int newX= i + x[k];
+            int newY= j + y[k];
             
-            if(r>=0 && r<rowSize && c >=0 && c<colSize && visited[r][c] == 0 && grid[r][c]== '1'){
-                dfs(grid,r,c,visited);
+            /* conditions:
+                1. newX should be greater >= 0 and newX < m(no of rows)
+                2. newY should be greater >= 0 and newY < n(no of rows)
+                3. cell should contain '1'
+                2. that cell should not be already visited
+
+                the conditions 1 and 2 are to avoid array out of bound exception
+            */
+
+            if(newX>=0 && newX<m && newY >=0 && newY<n && grid[newX][newY]== '1' && visited[newX][newY] == 0){
+                dfs(newX,newY,m,n,visited,grid);
             }
         }
         
